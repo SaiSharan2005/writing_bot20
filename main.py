dfe112172b1451eade522931f4d5b58ec24f5f12
SaiSharan2005 <duginisaisharan@gmail.com>
2022-12-04 00:34:48 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pyautogui,time
+l1 = []
+print("Paste the code here")
+while(True):
+	str = input("")
+
+	if str.startswith("    "):
+		str = str.replace("    ","")
+	if str == "exit":
+		break
+	l1.append(str)
+		
+time.sleep(5)
+for i in l1:
+	pyautogui.typewrite(i)
+	pyautogui.press("enter")
