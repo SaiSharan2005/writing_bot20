4ea2c9ef0b2c0c4539c60f906781a52b0b842619
Sai Sharan <108337097+SaiSharan2005@users.noreply.github.com>
2022-12-15 08:41:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
 import pyautogui,time
 l1 = []
+
 print("Paste the code here")
 while(True):
 	str = input("")
 
-	if str.startswith("    "):
-		str = str.replace("    ","")
+	while(str.startswith("\t") or str.startswith("  ")):
+		str = str.replace("\t","")
+		str = str.replace("  ","")
+
+
 	if str == "exit":
 		break
-	l1.append(str)
+	if '<' in str:
+		str1,str2= str.split("<")
+		l1.append(str1)
+		l1.append('<')
+		l1.append(str2)
+		boo =0
+	else:
+		l1.append(str)
 		
-time.sleep(3000)
-for i in l1:
-	pyautogui.typewrite(i)
-	pyautogui.press("enter")
+time.sleep(5)
+for j,i in enumerate(l1):
+	if i =='<':
+		pyautogui.keyDown("<")
+		time.sleep(0.59)
+		pyautogui.keyUp("<")
+		pyautogui.press("left")
+		pyautogui.press("backspace")
+		pyautogui.press("right")
+
+	else:
+		if l1[j+1]=='<':
+			pyautogui.typewrite(i)
+		else:
+			pyautogui.typewrite(i)
+			pyautogui.press("enter")
